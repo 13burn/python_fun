d23cebf2b73ef27e8053b6a9c8c8927cf136650d
Bardo Espinosa <bardoespinosaa@gmail.com>
2021-01-19 13:25:45 -0600

    fix and comments
    Fixxed a counter issue and added some comments

--- timer/timer.py
+++ timer/timer.py
@@ -2,11 +2,11 @@ import eel
 
 eel.init("front")
 
+#TODO: change this whole function to JS
 @eel.expose
 def converter(minutes, seconds):
     minute = int(minutes) * 60
-    second = minute + int(seconds)
+    second = minute + int(seconds)#i'm pretty sure js is sending ints but i haven'ttest it yet
     eel.countDown(second)
-    print("python ok")
     
 eel.start("front.html")
